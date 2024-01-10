b2c52f300a3a61791a6b089260a0ef331a258c81
GuilhermeSerafim <guilerstudies@gmail.com>
2024-01-10 13:26:00 -0300

    Criando função async para requisição POST, e convertendo corpo da resposta em string com a api JSON

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -4,6 +4,26 @@ async function listarVideosDaApi() {
     return conexaoConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json" // Que tipo de arquivo que está sendo enviado
+        },
+        body: JSON.stringify({ // Transformando corpo em uma string tudo que estamos enviando com api JSON
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
+
 export const conectaApi = {
-    listarVideosDaApi
+    listarVideosDaApi,
+    criaVideo
 };
