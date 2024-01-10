b3af884f4f822f7749f5c9b1293d8f406f1e6e47
GuilhermeSerafim <guilerstudies@gmail.com>
2024-01-10 14:00:13 -0300

    Criando elemento html video com base nas informações passadas na criação de video e enviando o mesmo para a api e assim, exibindo na nossa pagina de videos

--- js/criaVideo.js
+++ js/criaVideo.js
@@ -1,14 +1,18 @@
+import { conectaApi } from "./conectaApi.js";
+
 const formulario = document.querySelector("[data-formulario]");
 
-function criarVideo(evento) {
+async function criaElementoVideo(evento) {
     evento.preventDefault();
     const imagem = document.querySelector("[data-imagem]").value;
     const url =  document.querySelector("[data-url]").value;
     const titulo =  document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 10).toString(); // O total de visualizações é randomico    
+    // Essa função é async, então retorna uma promisse, para tratar isso, precisamos colocar await/async tbm
+    await conectaApi.criaVideo(titulo, descricao, url, imagem); 
 
-    console.log(imagem, url, titulo, descricao)
+    window.location.href = "../pages/envio-concluido.html";
 
 }
 
-formulario.addEventListener('submit', evento => criarVideo(evento));
+formulario.addEventListener('submit', evento => criaElementoVideo(evento));
