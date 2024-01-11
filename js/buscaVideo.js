ab85254586451c81fa9e99b223d7290ee83eb538
GuilhermeSerafim <guilerstudies@gmail.com>
2024-01-11 11:19:25 -0300

    Removendo todos os videos, logo em seguida adicionando os videos com base no valor do input de pesquisa

--- js/buscaVideo.js
+++ js/buscaVideo.js
@@ -1,17 +1,23 @@
 import { conectaApi } from "./conectaApi.js";
 import constroiCardLi from "./mostrarVideos.js";
 
-const pesquisarVideoInput = document.querySelector("[data-pesquisar-input]");
-const botaoDePesquisarVideo = document.querySelector("[data-pesquisar-botao]");
+const inputPesquisar = document.querySelector("[data-pesquisar-input]");
+const btPesquisar = document.querySelector("[data-pesquisar-botao]");
 
-botaoDePesquisarVideo.addEventListener('click', evento => buscaVideo(evento));
+btPesquisar.addEventListener('click', evento => buscaVideo(evento));
 
 async function buscaVideo(eventoDoBotao) {
     eventoDoBotao.preventDefault();
     // Realizando requisição get e abstraindo n objetos, conforme o valor da busca
-    const buscaNaApiComOValorDoInput = await conectaApi.buscaVideo(pesquisarVideoInput.value);
-
+    const buscaNaApiComOValorDoInput = await conectaApi.buscaVideo(inputPesquisar.value);
     const ulVideos = document.querySelector("[data-lista]");
+
+    // Removendo todos os vídeos
+    while(ulVideos.firstChild) {
+        ulVideos.removeChild(ulVideos.firstChild);
+    }
+
+    // Adicionando apenas os vídeos filtrados
     buscaNaApiComOValorDoInput.forEach(video => ulVideos.appendChild(
         constroiCardLi(video.titulo, video.descricao, video.url, video.imagem)));
 
